fb5732c3bc71f6193691eeb1244e3cbc44cf38ee
STokarew <Sseregatokarew@gmail.com>
2021-10-09 13:47:54 +0700

    Добавьте файлы проекта.

---
+++ REC-66/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace REC_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            short s = 2;
+            ushort us = 3;
+            char c = 'a';
+            bool flag = true;
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.WriteLine(us);
+            Console.WriteLine(c);
+            Console.WriteLine(flag);
+            Console.ReadKey();
+        }
+    }
+}
